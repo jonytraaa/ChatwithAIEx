9d80c8c37e2005e9c57513aa91d12387d576a0f8
jonytraaa <91388969+jonytraaa@users.noreply.github.com>
2025-02-05 17:51:31 +0100

    Delete background.js

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "openAIPage") {
-        chrome.tabs.create({ url: message.url }, (tab) => {
-            chrome.storage.local.set({ lastPrompt: message.prompt }); // Guarda el prompt temporalmente
-        });
-    }
-});
